1eff4d2d0000b4976f45cd0a39d506a81d6f5d4d
jianhuashao <jianhua.shao1986@gmail.com>
2012-06-21 21:15:12 +0100

    move some code for visitation in models.py from v0.2.1 into a view.py

--- libauth/models.py
+++ libauth/models.py
@@ -104,74 +104,26 @@ class Registration(models.Model):
     registrant_callback = models_type_callback()
     registrant_request_token = models_type_token()
     registrant_request_scope = models_type_access_scope()
+    registrant_request_reminder = models_type_reminder()
+    registrant_request_user_public = models_type_request_user_public() # not used at moment
+    registrant_redirect_token = models_type_token()
+    registrant_access_token = models_type_token()
+    registrant_access_validate = models_type_validate_code()
 
     register_callback = models_type_callback()
     register_request_token = models_type_token()
     register_request_scope = models_type_access_scope()
-
-    registrant_redirect_token = models_type_token()
+    register_request_reminder = models_type_reminder()
+    register_request_user_public = models_type_request_user_public() # not used at moment
     register_redirect_token = models_type_token()
-
-    registrant_access_token = models_type_token()
-    registrant_access_validate = models_type_validate_code()
-    
     register_access_token = models_type_token()
     register_access_validate = models_type_validate_code()
 
-    registrant_request_reminder = models_type_reminder()
-    register_request_reminder = models_type_reminder()
-    registrant_request_user_public = models_type_request_user_public()
-    register_request_user_public = models_type_request_user_public()
     registrant_request_media = models.IntegerField(max_length=2, choices=REQUEST_MEDIA_CHOICES, default=1) # default is desktop_browser
     register_request_media = models.IntegerField(max_length=2, choices=REQUEST_MEDIA_CHOICES, default=1) # default is desktop_browser
 
     def __unicode__(self):
         return '{Registrant(%s) | Register(%s)}'%(self.registrant_callback, self.register_callback)
-
-
-class regist_dealer(object):
-    def __init__(self, request):
-        self.request = request
-        self.user = request.user
-    def regist_init(self): pass
-    def registrant_request(self): pass
-    def register_owner_redirect(self): pass
-    def register_owner_grant(self): pass
-    def register_grant(self): pass
-    def registrant_owner_redirect(self): pass
-    def registrant_owner_grant(self): pass
-    def registrant_confirm(self): pass
-    def register_activate(self): pass
-    def regist_finish(self): pass
-
-
-def regist_steps(regist_dealer, request):
-    regist_status = request_get(request.REQUEST, url_keys.regist_status)
-    if regist_status == None:
-        return error_response(1, url_keys.regist_status)
-    if regist_status == REGIST_STATUS.init: # may not be necessary
-        #@login_required
-        return regist_dealer.regist_init()
-    if regist_status == REGIST_STATUS.registrant_request:
-        return regist_dealer.registrant_request()
-    if regist_status == REGIST_STATUS.register_owner_redirect:
-        #@login_required
-        return regist_dealer.register_owner_redirect()
-    if regist_status == REGIST_STATUS.register_owner_grant:
-        return regist_dealer.register_owner_grant()
-    if regist_status == REGIST_STATUS.register_grant:
-        return regist_dealer.register_grant()
-    if regist_status == REGIST_STATUS.registrant_owner_redirect:
-        return regist_dealer.registrant_owner_redirect()
-    if regist_status == REGIST_STATUS.registrant_owner_grant:
-        return regist_dealer.registrant_owner_grant()
-    if regist_status == REGIST_STATUS.registrant_confirm:
-        return regist_dealer.registrant_confirm()
-    if regist_status == REGIST_STATUS.register_activate:
-        return regist_dealer.register_activate()
-    if regist_status == REGIST_STATUS.regist_finish: ## may not be necessary
-        return regist_dealer.regist_finish()
-    return error_response(2, (url_keys.regist_status, regist_status))
 
 
 def find_key_by_value_regist_type(regist_type):
