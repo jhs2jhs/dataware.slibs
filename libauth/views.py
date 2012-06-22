040e430c387c304320f882e5a4c8a89a44c8cbbc
jianhuashao <jianhua.shao1986@gmail.com>
2012-06-22 22:19:10 +0100

    clear method_registrant_request()

--- libauth/views.py
+++ libauth/views.py
@@ -75,31 +75,18 @@ def method_regist_init(request):
 ####
 @login_required
 def method_registrant_request(request, regist_callback_me):
-    user = request.user # user has to login, so that it would reduce the man-in-the middle attack
+    user = request.user # user has to login, so that it would reduce the man-in-the middle attack, only logined user can make request, it will reduce unknown attack. We can also write code to limit the frequency of user to request, so that we can provide a health API over there. 
     register_callback = request_get(request.REQUEST, url_keys.regist_callback)
     regist_type = request_get(request.REQUEST, url_keys.regist_type)
     registrant_request_scope = request_get(request.REQUEST, url_keys.registrant_request_scope)
     registrant_request_reminder = request_get(request.REQUEST, url_keys.registrant_request_reminder)
-    registrant_request_user_public = request_get(request.REQUEST, url_keys.registrant_request_user_public) # it is not compuslary at monent
-    print register_callback, regist_type, registrant_request_scope, registrant_request_reminder, registrant_request_user_public
+    registrant_request_user_public = request_get(request.REQUEST, url_keys.registrant_request_user_public)
     if (check_compulsory((register_callback, regist_type, registrant_request_scope, registrant_request_reminder))) == False:
         return error_response(5, ())
     if (check_choice(REGIST_TYPE, regist_type)) == False:
         return error_response(2, (url_keys.regist_type, regist_type))
     ##
-    registrant_request_token = dwlib.token_create_user(register_callback, regist_callback_me, TOKEN_TYPE['request'], user) # should I user.id or just user here?
-    ##
-    params = {
-        url_keys.regist_status: REGIST_STATUS['register_owner_redirect'], #
-        url_keys.regist_type: regist_type,
-        url_keys.regist_callback: regist_callback_me,
-        url_keys.registrant_request_token: registrant_request_token,
-        url_keys.registrant_request_scope: registrant_request_scope,
-        url_keys.registrant_request_reminder: registrant_request_reminder,
-        url_keys.registrant_request_user_public: registrant_request_user_public,
-        }
-    url_params = dwlib.urlencode(params)
-    url = '%s?%s'%(register_callback, url_params) # attack: if one takes a correct user session, will it keep request here to explore the database? whether it should set up the protection to protect the register speed?
+    registrant_request_token = dwlib.token_create_user(register_callback, regist_callback_me, TOKEN_TYPE['request'], user) 
     ##
     regist_type_key = find_key_by_value_regist_type(regist_type) ##what happened if not correct here?
     regist_status_key = find_key_by_value_regist_status(REGIST_STATUS['registrant_request']) 
@@ -113,10 +100,23 @@ def method_registrant_request(request, regist_callback_me):
         registrant_request_reminder=registrant_request_reminder, 
         registrant_request_user_public=registrant_request_user_public,
         user=user)
+    ## #how to check whether a request token is in the working status, you can check whehter regist_status >= register_owner_redirect, better to set up cron to do it
+    params = {
+        url_keys.regist_status: REGIST_STATUS['register_owner_redirect'], #
+        url_keys.regist_type: regist_type,
+        url_keys.regist_callback: regist_callback_me,
+        url_keys.registrant_request_token: registrant_request_token,
+        url_keys.registrant_request_scope: registrant_request_scope,
+        url_keys.registrant_request_reminder: registrant_request_reminder,
+        url_keys.registrant_request_user_public: registrant_request_user_public,
+        }
+    url_params = dwlib.urlencode(params)
+    url = '%s?%s'%(register_callback, url_params) 
     ##
     c = get_context_base_regist()
     c['regist_redirect_url']['value'] = url
     c['regist_status']['value'] = REGIST_STATUS['register_owner_redirect'] # do I need to pass it in?
+    c['regist_status_current']['value'] = REGIST_STATUS['registrant_request']
     c['regist_type']['value'] = regist_type
     context = RequestContext(request, c)
     return render_to_response('registrant_request.html', context)
@@ -129,7 +129,7 @@ def method_register_owner_redirect(request, regist_callback_me):
     registrant_request_token = request_get(request.REQUEST, url_keys.registrant_request_token)
     registrant_request_scope = request_get(request.REQUEST, url_keys.registrant_request_scope) # may check it is in scope or not
     registrant_request_reminder = request_get(request.REQUEST, url_keys.registrant_request_reminder)
-    registrant_request_user_public = request_get(request.REQUEST, url_keys.registrant_request_user_public) # may not be compulsory
+    registrant_request_user_public = request_get(request.REQUEST, url_keys.registrant_request_user_public) # may not be compulsory TODO it is requesd to a specific user, it can add to use later. 
     #print register_callback, regist_type, registrant_request_scope, registrant_request_reminder, registrant_request_user_public
     if (check_compulsory((registrant_callback, regist_type, registrant_request_token, registrant_request_scope, registrant_request_reminder))) == False:
         return error_response(5, ())
